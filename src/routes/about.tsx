d6d1a53a1209ac2724f09fa082c8ebfc42594a35
Raitis Sondors <153851207+dzinulis@users.noreply.github.com>
2024-05-25 22:47:10 +0300

    Update about.tsx

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,11 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/about')({
-  component: () => <div>
+  component: () => 
+  <div>
     <p>Atlasi vārdu no latviešu vārda dienu kalendāra.</p>
+    <p>Lapas kods atrodams <a href="https://github.com/dzinulis/name-finder">šeit</a></p>
     <p>Autors Raitis Sondors</p>
-    <p>GitHub: <a href="https://github.com/dzinulis">https://github.com/dzinulis</a> </p>
+    <p>GitHub: <a href="https://github.com/dzinulis">https://github.com/dzinulis</a></p>
     <p>LinkedIn: <a href="https://www.linkedin.com/in/raitis-sondors/">https://www.linkedin.com/in/raitis-sondors/</a> </p>
-
   </div>
 })
